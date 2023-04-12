e0d9fcdfbfcdbb231092a2ed081de14bc20bfa66
unknown <Riyanagwl@gmail.com>
2023-04-12 14:25:40 +0300

    Add jwt and devise as an authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  has_many :doctors, dependent: :destroy
+  has_many :appointments, dependent: :destroy
+  validates :username, presence: true, uniqueness: true
+end
---
+++ db/migrate/20230412104346_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.integer :role, default: 0
+
+      t.timestamps
+    end
+  end
+end
+
