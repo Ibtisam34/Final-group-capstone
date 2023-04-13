6c4a22e7bd218e7672e111a3502068f476472dfd
girma <girmatarekegn.gi@gmail.com>
2023-04-13 12:15:25 +0300

    Add appointment model

---
+++ app/models/appointment.rb
@@ -0,0 +1,7 @@
+class Appoinment < ApplicationRecord
+  belongs_to :user
+  belongs_to :doctor
+
+  validates :pickup_date, :return_date presence: true
+  validates :pickup_date, :return_date datetime: true
+end
