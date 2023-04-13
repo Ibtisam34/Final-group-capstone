c0019c8df23cbd79906db5ce8dbee18d3ef4dc06
girma <girmatarekegn.gi@gmail.com>
2023-04-13 12:22:23 +0300

    Fixed rubocop error

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,6 @@ class Appoinment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
 
-  validates :pickup_date, :return_date presence: true
-  validates :pickup_date, :return_date datetime: true
+  validates :pickup_date, :return_date, presence: true
+  validates :pickup_date, :return_date, datetime: true
 end
