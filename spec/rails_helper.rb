b5e34837575a2df402fb2656c8dc52f6fb8b7ae0
odai alazzeh <odai.alazzeh@gmail.com>
2023-04-13 15:29:24 +0200

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 
 require 'shoulda/matchers'
 require 'factory_bot_rails'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
