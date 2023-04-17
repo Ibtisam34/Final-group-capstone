5032476657169e16bced18b25741da4a124ab33d
girma <girmatarekegn.gi@gmail.com>
2023-04-17 15:35:14 +0300

    Add Read me and authorization using jwtDenyList

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,20 @@
+class MembersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    user = gets_user_from_token
+    render json: {
+      message: "If you see this, you're in!",
+      user:
+    }, status: :ok
+  end
+
+  private
+
+  def gets_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split[1],
+                             Rails.application.credentials.devise[:jwt_secret_key]).first
+    user_id = jwt_payload['sub']
+    User.find(user_id.to_s)
+  end
+end
