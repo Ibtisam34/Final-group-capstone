015f34f36fa174bc9d91d1d7d75a3668cc90e5bd
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-21 09:35:25 +0100

    Add Tests

--- app/models/apponitment.rb
+++ app/models/apponitment.rb
@@ -1,6 +1,6 @@
 class Apponitment < ApplicationRecord
-    belongs_to :user
-    belongs_to :doctor
+  belongs_to :user
+  belongs_to :doctor
 
-    validates :date, presence: true
+  validates :date, presence: true
 end
